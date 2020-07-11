50189a1bf6f2b48b2ccbe5af9601c10b4fb45151
drachels <drachels@drachels.com>
2020-07-11 13:28:07 -0500

    Added manager to write capability.

--- db/access.php
+++ db/access.php
@@ -36,7 +36,8 @@ $capabilities = array(
         'archetypes' => array(
             'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
         )
     ),
 
