9fbed6dc3bc5293f34439a53e8105352ad3101c0
AL Rachels <drachels@drachels.com>
2021-06-12 14:31:35 -0500

    Update version.php
    Bumped version.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021060900;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2021061200;      // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015111600;      // Requires Moodle 3.0 version.
 $plugin->cron      = 60;              // Period for cron to check this module (secs).
 $plugin->component = 'mod_diary';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.3.2 (Build: 2021060900)'; // User-friendly version number.
+$plugin->release   = '3.3.3 (Build: 2021061200)'; // User-friendly version number.
