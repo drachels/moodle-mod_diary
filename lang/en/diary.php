fb04a332378d85e57fbb74d4e08a78922d328497
AL Rachels <drachels@drachels.com>
2024-05-27 16:08:48 -0500

    deprecated strings

--- lang/en/diary.php
+++ lang/en/diary.php
@@ -471,3 +471,25 @@ $string['writingpromptlable3'] = 'Writing Prompt Editor';
 $string['writingpromptnotused'] = 'The normal diary settings were used for this entry\'s auto-rating percent settings.';
 $string['writingpromptused'] = 'Writing prompt ID: {$a} was used for this entry\'s auto-rating percent settings.';
 $string['xfrresults'] = 'There were {$a->one} entry\'s processed, and {$a->two} of them transferred.';
+
+// Deprecated since Moodle 4.3.
+$string['grade,mod_diary'] = 'Grade';
+$string['itemcount_help'] = 'The minimum number of countable items that must be in the essay text
+    in order to achieve the maximum rating for this entry.
+
+Note, that this value may be rendered ineffective by the rating bands, if any, defined below.';
+$string['itemcount'] = 'Expected number of items';
+$string['itempercent_help'] = 'Select the percentage of total rating that should be deducted for each missing countable item.';
+$string['itempercent'] = 'Penalty per item';
+$string['itemtype_desc'] = 'The type of items in the essay text that will contribute to the auto-rating is,
+    <strong>{$a->one}</strong>, and for full marks you must use, <strong>{$a->two}</strong>, of them.';
+$string['itemtype_descr'] = 'Select the type of items in the essay text that will contribute to the auto-rating.';
+$string['itemtype_help'] = 'Select the type of items in the essay text that will contribute to the auto-rating.';
+$string['itemtype_title'] = 'Type of countable items';
+$string['itemtype'] = 'Type of countable items';
+$string['itemtypefiles'] = 'Files';
+$string['itemtypechars'] = 'Characters';
+$string['itemtypenone'] = 'None';
+$string['itemtypensentences'] = 'Sentences';
+$string['itemtypeparagraphs'] = 'Paragraphs';
+$string['itemtypewords'] = 'Words';
