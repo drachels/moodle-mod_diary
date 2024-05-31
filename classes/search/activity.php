1c9b8f2c7bde7f2837381b1b8ed3d5b0bfd54ec3
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-05-31 12:44:26 -0700

    Turn off global search file indexing.
    the current implementation is broken due to the usage of an undefined
    constant.
    turning this off fixes and removing the broken function fixes this issue.

--- classes/search/activity.php
+++ classes/search/activity.php
@@ -40,21 +40,6 @@ class activity extends \core_search\base_activity {
      * @return bool
      */
     public function uses_file_indexing() {
-        return true;
-    }
-
-    /**
-     * Return the context info required to index files for
-     * this search area.
-     *
-     * @return array
-     */
-    public function get_search_fileareas() {
-        $fileareas =
-            [
-                'intro',
-                DIARY_INTROATTACHMENT_FILEAREA,
-            ]; // Fileareas.
-        return $fileareas;
+        return false;
     }
 }
