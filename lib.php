53fbccb9f8a8d63c968940540550fa6d9944ccff
Lai Wei <lai.wei@enovation.ie>
2024-08-21 10:28:51 +0100

    Push assesstimestart and assesstimefinish when resetting a course

--- lib.php
+++ lib.php
@@ -607,7 +607,7 @@ function diary_reset_userdata($data) {
     if ($data->timeshift) {
         // Any changes to the list of dates that needs to be rolled should be same during course restore and course reset.
         // See MDL-9367.
-        shift_course_mod_dates('diary', ['timeopen', 'timeclose'], $data->timeshift, $data->courseid);
+        shift_course_mod_dates('diary', ['timeopen', 'timeclose', 'assesstimestart', 'assesstimefinish'], $data->timeshift, $data->courseid);
         $status[] = ['component' => $componentstr, 'item' => get_string('datechanged'), 'error' => false];
     }
 
