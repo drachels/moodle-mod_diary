c1ef5c219c441671a5d5446c68ba0f44f4b12d48
AL Rachels <drachels@drachels.com>
2025-08-09 13:49:57 -0500

    Update tasks.php

--- db/tasks.php
+++ db/tasks.php
@@ -23,7 +23,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die(); // @codingStandardsIgnoreLine
+defined('MOODLE_INTERNAL') || die(); // phpcs:ignore
 
 $tasks = [
     [
