0fcc6fa4bb65915d8c37d67b2cedf858a66a683e
AL Rachels <drachels@drachels.com>
2026-02-06 13:31:36 -0600

    Update entry_deleted.php

--- classes/event/entry_deleted.php
+++ classes/event/entry_deleted.php
@@ -1,84 +1,36 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// ... (header and namespace unchanged)
 
-/**
- * The mod_diary entry deleted event.
- *
- * @package   mod_diary
- * @copyright 2025 drachels@drachels.com
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-namespace mod_diary\event;
-
-defined('MOODLE_INTERNAL') || die(); // phpcs:ignore
-
-/**
- * The mod_diary entry deleted class.
- *
- * @property-read array $other {
- *      Extra information about event.
- *
- *      - int USER: the userid of the entry.
- *      - int entryid: the id of the diary entry.
- * }
- * @package   mod_diary
- * @since     Moodle 2.7
- * @copyright 2025 drachels@drachels.com
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 class entry_deleted extends \core\event\base {
 
-    /**
-     * Init method.
-     */
     protected function init() {
-        $this->data['crud'] = 'u';
-        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
-        $this->data['objecttable'] = 'diary';
+        $this->data['crud']         = 'd';
+        $this->data['edulevel']     = self::LEVEL_PARTICIPATING;
+        $this->data['objecttable']  = 'diary_entries';
     }
 
-    /**
-     * Returns localised general event name.
-     *
-     * @return string
-     */
     public static function get_name() {
         return get_string('evententrydeleted', 'mod_diary');
     }
 
-    /**
-     * Returns description of what happened.
-     *
-     * @return string
-     */
     public function get_description() {
-        return "The user with id '$this->userid' has deleted an entry for the diary activity with the course module id
-            '$this->contextinstanceid'";
+        return "The user with id '{$this->userid}' deleted diary entry '{$this->objectid}' "
+             . "belonging to user '{$this->relateduserid}' in the Diary activity "
+             . "with course module id '{$this->contextinstanceid}'.";
     }
 
-    /**
-     * Returns relevant URL.
-     *
-     * @return \moodle_url
-     */
     public function get_url() {
-        return new \moodle_url('/mod/diary/view.php',
-            [
-                'id' => $this->contextinstanceid,
-            ]
-        );
+        return new \moodle_url('/mod/diary/view.php', ['id' => $this->contextinstanceid]);
+    }
+
+    protected function validate_data() {
+        parent::validate_data();
+
+        if (!isset($this->relateduserid)) {
+            throw new \coding_exception('The \'relateduserid\' must be set.');
+        }
+        if (!isset($this->objectid)) {
+            throw new \coding_exception('The \'objectid\' must be set to the deleted entry ID.');
+        }
     }
 }
