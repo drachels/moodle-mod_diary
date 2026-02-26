05366ca1c6461bf70d38e30f44eab5a7456e073f
AL Rachels <drachels@drachels.com>
2026-02-26 16:26:26 -0600

    Update version.php

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026020603; // The current module version (Date: YYYYMMDDXX).
-// $plugin->requires  = 2024100700;  // Moodle 4.5.0 release number.
+$plugin->version   = 2026022600; // The current module version (Date: YYYYMMDDXX).
+$plugin->requires  = 2024100700;  // Moodle 4.5.0 release date, October 7, 2024..
 $plugin->cron      = 60; // Period for cron to check this module (secs).
 $plugin->component = 'mod_diary';
-$plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '4.0.0 (Build: 2026020603)'; // User-friendly version number.
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '4.0.0 (Build: 2026022600)'; // User-friendly version number.
 $plugin->supported = [405, 501];
